15e39ea3e23bca46f40a43fff33f1b2307a34e0a
Nicolas The Boss <noudard+theboss@weborama.com>
2015-10-24 23:21:02 +0200

    add the calculator process

--- Angular/js/controller/ComputeController.js
+++ Angular/js/controller/ComputeController.js
@@ -1,3 +1,43 @@
 app.controller('ComputeController', function($scope){
   $scope.result = false;
+  $scope.newCompute = {};
+  $scope.lastResult = "Please fill the fields";
+  $scope.computes = [];
+  $scope.computeProcess = function(){
+    console.log($scope.newCompute.operation)
+    if ($scope.newCompute.operation == undefined) {
+      $scope.lastResult = "fill the operation select please";
+      return;
+    }
+    if ($scope.newCompute.numbera == undefined || $scope.newCompute.numberb == undefined) {
+      $scope.lastResult = "fill the numbers";
+      return;
+    }
+    switch ($scope.newCompute.operation)
+    {
+      case "sum":
+        $scope.newCompute.result = $scope.newCompute.numbera*1 + $scope.newCompute.numberb*1;
+        $scope.newCompute.op = "+";
+        break;
+      case "substract":
+        $scope.newCompute.result = $scope.newCompute.numbera*1 - $scope.newCompute.numberb*1;
+        $scope.newCompute.op = "-";
+        break;
+      case "multiply":
+        $scope.newCompute.result = $scope.newCompute.numbera*1 * $scope.newCompute.numberb*1;
+        $scope.newCompute.op = "*";
+        break;
+      case "divide":
+        if ($scope.newCompute.numberb == "0") {
+          $scope.lastResult = "divide by 0 is forbidden";
+          return;
+        }
+        $scope.newCompute.result = $scope.newCompute.numbera / $scope.newCompute.numberb;
+        $scope.newCompute.op = "/";
+        break;
+    }
+    $scope.lastResult = "Last result is : " + $scope.newCompute.result;
+    $scope.computes.push($scope.newCompute);
+    $scope.newCompute = {};
+  }
 })
